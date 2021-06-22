b63fb1b2e88e35d790fd94b184a60e5c4a9c4d2e
Jogius <jogius.julius.makowski@gmail.com>
2021-06-22 14:31:58 +0200

    InfluxDB Setup

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 from dotenv import load_dotenv
 from os import environ
+from influxdb import InfluxDBClient
+
+def getDatabaseClient(host, port):
+  return InfluxDBClient(host=host, port=port, database='solaranzeige')
 
 def main():
   HOST = environ['DB_HOST']
   PORT = environ['DB_PORT']
   INTERVAL = environ['QUERY_INTERVAL']
+
+  client = getDatabaseClient(HOST, PORT)
+
+  client.close()
 
 if __name__ == '__main__':
   load_dotenv()
