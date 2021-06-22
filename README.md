646f8e39d80cdabeb51dd24a3d348a98f3e0cb4d
Jogius <jogius.julius.makowski@gmail.com>
2021-06-22 15:33:49 +0200

    Email sending

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # solaranzeige-mailer
+
 A python program that reads data from a solaranzeige influxdb and sends an email if certain conditions are met
 
 ## Disclaimer
+
 This script is can be used with a software called [solaranzeige](https://solaranzeige.de). However, it was created for my personal use and is not actively maintained, so compatibility with newer versions of solaranzeige is likely but not promised.
 
 ## Configuration
-To configure the script, simply copy the ``.env.example`` file to ``.env`` and replace the values with your own.
+
+To configure the script, simply copy the `.env.example` file to `.env` and replace the values with your own.
+
+## Description
+
+The script averages the data from solaranzeige in the last x minutes and checks for a certain case: if more energy is produced than used, there is room left in the battery and too little of that leftover power is going to the battery, an email is sent to notify the user.
--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 from os import environ
 from influxdb import InfluxDBClient
+from smtplib import SMTP_SSL as SMTP
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 def getDatabaseClient(host, port):
   return InfluxDBClient(host=host, port=port, database='solaranzeige')
@@ -20,8 +23,31 @@ def getAverages(client, interval):
     'socAvg': next(result[3].get_points(measurement='Batterie'))['mean']
   }
 
-def handleProblem(averages, plus, difference):
-  pass
+def getSMTPConnection(host, port, username, password):
+  connection = SMTP(host, port)
+  connection.login(username, password)
+  return connection
+
+def handleProblem(averages, plus, difference, data):
+  HOST = environ['SMTP_HOST']
+  PORT = environ['SMTP_PORT']
+  USERNAME = environ['SMTP_USERNAME']
+  PASSWORD = environ['SMTP_PASSWORD']
+  SENDER = environ['MAIL_SENDER']
+  RECIPIENT = environ['MAIL_RECIPIENT']
+  SUBJECT = environ['MAIL_SUBJECT']
+
+  email = MIMEMultipart()
+  email['From'] = SENDER
+  email['To'] = RECIPIENT
+  email['Subject'] = SUBJECT
+  email.attach(MIMEText('Sehr geehrte Herr Makowski,\ndie Sonnenbatterie macht wieder Mist, siehe folgende Daten.\n\n' + data + '\n\nLiebe Grüße,\nPython'))
+
+  mailServer = getSMTPConnection(HOST, PORT, USERNAME, PASSWORD)
+
+  mailServer.sendmail(SENDER, RECIPIENT, email.as_string())
+
+  mailServer.quit()
 
 def main():
   HOST = environ['DB_HOST']
@@ -44,18 +70,17 @@ def main():
   # and how much is actually going in
   difference = abs(plus - averages['batteryAvg'])
 
+  # String with important data
+  data = f' - plus: {plus}\n - batteryAvg: {averages["batteryAvg"]}\n --> difference: {difference}\n - socAvg: {averages["socAvg"]}'
+
   # Logging
-  print(f'--------------------\n'
-    ' - plus: {plus}\n'
-    ' - batteryAvg: {averages["batteryAvg"]}\n'
-    ' --> difference: {difference}\n'
-    ' - socAvg: {averages["socAvg"]}')
+  print(f'------------------------------\n' + data)
   
   # If a certain amount of energy is left over, the battery has less than a certain
   # percentage of power and the difference between this leftover and how much is going
   # into the battery is too big, call a function to handle the problem case
   if plus >= MIN_PLUS and averages['socAvg'] <= MIN_SOC and difference > MAX_DIFFERENCE:
-    handleProblem(averages, plus, difference)
+    handleProblem(averages, plus, difference, data)
 
   client.close()
 
