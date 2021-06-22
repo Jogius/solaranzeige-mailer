d00c01542caf69de16adc76ed23af4f37ccfd1c6
Jogius <jogius.julius.makowski@gmail.com>
2021-06-22 14:34:19 +0200

    Average queries

--- src/main.py
+++ src/main.py
@@ -5,12 +5,29 @@ from influxdb import InfluxDBClient
 def getDatabaseClient(host, port):
   return InfluxDBClient(host=host, port=port, database='solaranzeige')
 
+def getAverages(client, interval):
+  result = client.query(f'''
+    SELECT MEAN(Verbrauch) FROM AC WHERE time > now() - {interval}m;
+    SELECT MEAN(Leistung) FROM PV WHERE time > now() - {interval}m;
+    SELECT MEAN(Leistung) FROM Batterie WHERE time > now() - {interval}m;
+    SELECT MEAN(User_SOC) FROM Batterie WHERE time > now() - {interval}m;
+  ''')
+
+  return {
+    'consumptionAvg': next(result[0].get_points(measurement='AC'))['mean'],
+    'solarAvg': next(result[1].get_points(measurement='PV'))['mean'],
+    'batteryAvg': -(next(result[2].get_points(measurement='Batterie'))['mean']),
+    'socAvg': next(result[3].get_points(measurement='Batterie'))['mean']
+  }
+
 def main():
   HOST = environ['DB_HOST']
   PORT = environ['DB_PORT']
   INTERVAL = environ['QUERY_INTERVAL']
 
   client = getDatabaseClient(HOST, PORT)
+
+  averages = getAverages(client, INTERVAL)
 
   client.close()
 
